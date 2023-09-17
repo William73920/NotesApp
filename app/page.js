75f3e0da0273a2ca02699c1ff0048f07bebc639f
Will <rrozario958@gmail.com>
2023-09-17 18:30:25 +0530

    fixed loading

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
     }
   }, [currentUser, isLoading]);
 
-  return !isLoading && !currentUser ? (
+  return (!isLoading && !currentUser) || isLoading ? (
     <Loader />
   ) : (
     <main className={styles.body}>
